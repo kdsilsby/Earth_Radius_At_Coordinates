16fb1d08b198f711c7608424739179d65410269b
Kurt Silsby <kdsilsby@gmail.com>
2024-09-12 22:03:35 -0400

    Initial start on basic presentation of calculation of Earth's radius based on latitude. Basic input and output interface. Simple equation calculation.

--- Earth_Radius_At_Coordinates/ContentView.swift
+++ Earth_Radius_At_Coordinates/ContentView.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var latitude: Double = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("Latitude: ")
+                Spacer()
+                TextField(
+                    "Latitude",
+                    value: $latitude, 
+                    format: .number)
+                .border(.secondary)
+                .frame(
+                    minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,
+                    idealWidth: 70,
+                    maxWidth: .leastNormalMagnitude,
+                    minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,
+                    idealHeight: 12,
+                    maxHeight: .leastNormalMagnitude,
+                    alignment: .trailing)
+                Text("° North or South")
+            }
+            HStack {
+                Text("Earth Radius at " + String(format: "%.2f", latitude) + "° N/S:")
+                Spacer()
+                Text(
+                    String(
+                        format: "%.3f",
+                        geocentricRadius(latitude: latitude)
+                    )
+                )
+                Text("Km")
+            }
         }
         .padding()
     }
---
+++ Earth_Radius_At_Coordinates/Equations.swift
@@ -0,0 +1,20 @@
+//
+//  Equations.swift
+//  Earth_Radius_At_Coordinates
+//
+//  Created by Kurt Silsby on 9/12/24.
+//
+import Foundation
+
+
+func geocentricRadius(latitude: Double) -> Double {
+    let a = 6378.1370 //km
+    let b = 6356.7523 //km
+    
+    let lat_rad = latitude * Double.pi / 180
+    
+    let numerator = pow((pow(a, 2) * cos(lat_rad)),2) + pow((pow(b, 2) * sin(lat_rad)),2)
+    let denominator = pow(a * cos(lat_rad), 2) + pow(b * sin(lat_rad), 2)
+    
+    return sqrt(numerator/denominator)
+}
